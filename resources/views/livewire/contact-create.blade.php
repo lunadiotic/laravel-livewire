16a89dbd17ba3492cfedd6a6cda9b3ce148896ef
idstack <idstack@outlook.co.id>
2020-04-28 17:08:40 +0000

    memanggil component livewire dalam component

---
+++ resources/views/livewire/contact-create.blade.php
@@ -0,0 +1,15 @@
+<div>
+    <form action="#">
+        <div class="form-group">
+            <div class="form-row">
+                <div class="col">
+                    <input type="text" name="" id="" class="form-control" placeholder="Name">
+                </div>
+                <div class="col">
+                    <input type="text" name="" id="" class="form-control" placeholder="Phone">
+                </div>
+            </div>
+        </div>
+        <button type="submit" class="btn btn-sm btn-primary">Submit</button>
+    </form>
+</div>
