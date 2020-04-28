eb4175e6a974dc50a4388ad56cfd8e8cee7b78c7
idstack <idstack@outlook.co.id>
2020-04-28 17:51:52 +0000

    menampilkan flash message

--- resources/views/livewire/contact-index.blade.php
+++ resources/views/livewire/contact-index.blade.php
@@ -1,5 +1,11 @@
 <div>
 
+    @if (session()->has('message'))
+        <div class="alert alert-success">
+            {{ session('message') }}
+        </div>
+    @endif
+
     <livewire:contact-create></livewire:contact-create>
 
     <hr>
