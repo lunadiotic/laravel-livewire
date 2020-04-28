526a856a12b507c71ee9771a282ad66a410dce51
idstack <idstack@outlook.co.id>
2020-04-28 16:29:19 +0000

    menampilkan data ke table

--- resources/views/livewire/contact-index.blade.php
+++ resources/views/livewire/contact-index.blade.php
@@ -1,7 +1,27 @@
 <div>
-    <ul>
-        @foreach ($contacts as $contact)
-            <li>{{ $contact->name }}</li>
-        @endforeach
-    </ul>
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Phone</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $no = 0; ?>
+            @foreach ($contacts as $contact)
+            <?php $no++; ?>
+            <tr>
+                <th scope="row">{{ $no }}</th>
+                <td>{{ $contact->name }}</td>
+                <td>{{ $contact->phone }}</td>
+                <td>
+                    <button class="btn btn-sm btn-info text-white">Edit</button>
+                    <button class="btn btn-sm btn-danger text-white">Delete</button>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
