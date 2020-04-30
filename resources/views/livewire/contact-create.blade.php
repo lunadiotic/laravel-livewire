7a74d140e42a46ee84ceb71d76d9ef2ef6d84ff5
idstack <idstack@outlook.co.id>
2020-04-30 18:10:36 +0000

    menambahkan validasi input

--- resources/views/livewire/contact-create.blade.php
+++ resources/views/livewire/contact-create.blade.php
@@ -3,10 +3,30 @@
         <div class="form-group">
             <div class="form-row">
                 <div class="col">
-                    <input wire:model="name" type="text" name="" id="" class="form-control" placeholder="Name">
+                    <input wire:model="name"
+                    type="text" name="" id=""
+                    class="form-control @error('name') is-invalid @enderror"
+                    placeholder="Name">
+
+                    @error('name')
+                        <span class="invalid-feedback">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
                 <div class="col">
-                    <input wire:model="phone" type="text" name="" id="" class="form-control" placeholder="Phone">
+                    <input wire:model="phone"
+                    type="text"
+                    name=""
+                    id=""
+                    class="form-control @error('name') is-invalid @enderror"
+                    placeholder="Phone">
+
+                    @error('phone')
+                        <span class="invalid-feedback">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
             </div>
         </div>
